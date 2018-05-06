c67cb469e46135c160f0de0f21d040f227cbc07d
Isamu Yonekawa <8orange.pp@gmail.com>
2018-05-06 08:46:46 +0000

    create blog

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,4 +1,50 @@
 class BlogsController < ApplicationController
+  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+  
   def index
+    @blogs = Blog.order(:id)
+  end
+  
+  def new
+    @blog = Blog.new
+    render 'new'
+  end
+  
+  def create
+    @blog = Blog.new(blog_params)
+    if @blog.save
+      redirect_to blog_path(@blog.id)
+    else
+      render 'new'
+    end
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @blog.update(blog_params)
+      redirect_to blogs_path, notice: 'ブログを編集しました！'
+    else
+      render 'edit'
+    end
+  end
+  
+  def show
+    
+  end
+  
+  def destroy
+    @blog.destroy
+    redirect_to blogs_path
+  end
+  
+  private
+  def blog_params
+    params.require(:blog).permit(:title, :content)
+  end
+  
+  def set_blog
+    @blog = Blog.find(params[:id])
   end
 end
